d3e580864c25474b6323ec2297285a399b7a34ea
tcrensink <thomas.rensink@gmail.com>
2023-11-15 22:46:24 -0800

    revise installation script to use poetry

--- install.py
+++ install.py
@@ -2,6 +2,8 @@
 
 import json
 import os
+import subprocess
+import sys
 
 # This file asks user successive prompts; based on the result will update various code bits
 print("This script will guide you through installation.\n")
@@ -25,39 +27,58 @@ def set_openai_key(key_string):
         json.dump(json_data, fp, sort_keys=True, indent=4)
 
 
-def tmux_exists() -> bool:
-    """Detect if tmux is installed."""
-    if os.system("which tmux") == 0:
-        return True
-    return False
+def check_base_reqs() -> bool:
+    """Check if tmux, poetry are installed (required for installation)."""
+
+    return_val = True
+    poetry_return_code = subprocess.run(
+        ["poetry", "--version"], capture_output=True, text=True
+    ).returncode
+    if poetry_return_code != 0:
+        print("install poetry and rerun.")
+        return_val = False
+
+    tmux_return_code = subprocess.run(
+        ["tmux -V"], capture_output=True, text=True
+    ).returncode
+    if tmux_return_code != 0:
+        print("install tmux and rerun.")
+        return_val = False
+    return return_val
+
+
+def install_reqs():
+    """Install python requirements with poetry."""
+    output = subprocess.run(["poetry", "install"], capture_output=True, text=True)
+    print(output.stdout)
 
 
 def main():
     print("This script will guide you through installation.")
-    print("looking for tmux...")
-    if not tmux_exists():
-        print("Please install tmux and try again.")
+    if not check_base_reqs():
         return
 
     while True:
         if os.path.exists("secrets.json"):
-            raise Exception("secrets.json already exists; Delete it before proceeding.")
-        key_str = input(
-            f"\nEnter openai_api_key (required). This will be stored in {PROJECT_FOLDER}/secrets.json: "
-        )
-        yn_resp = input(f"is this correct? (y/n): {key_str}")
-        if yn_resp.lower() in ("yes", "y", ""):
-            set_openai_key(key_str)
-            break
+            print("secrets.json already exists, continuing...")
+        else:
+            key_str = input(
+                f"\nEnter openai_api_key (required). This will be stored in {PROJECT_FOLDER}/secrets.json: "
+            )
+            yn_resp = input(f"is this correct? (y/n): {key_str}")
+            if yn_resp.lower() in ("yes", "y", ""):
+                set_openai_key(key_str)
+                break
 
-    resp = input("Install requirements? (y/n): ")
+    resp = input("Install python requirements? (y/n): ")
     if resp.lower() in ("yes", "y", ""):
-        os.system("python -m pip install -r requirements.txt")
+        install_reqs()
         print("requirements installed.")
 
-
     while True:
-        shell_file = input("Provide a shell file that is sourced to add the `chat` function (e.g. ~/.bashrc, ~/.zshrc): ")
+        shell_file = input(
+            "Provide a shell file that is sourced to add the `chat` function (e.g. ~/.bashrc, ~/.zshrc): "
+        )
         shell_file = os.path.expanduser(shell_file)
         if os.path.exists(shell_file):
             try:
