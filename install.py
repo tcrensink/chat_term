ada9d434b90324ecca0bf7b84c9e44efe3ac6d49
tcrensink <thomas.rensink@gmail.com>
2025-04-05 08:28:11 -0700

    minor revision to install flow

--- install.py
+++ install.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python
 import json
 import os
 import subprocess
@@ -14,11 +13,15 @@ function chat() {{
 
 
 def set_openai_key(key_string):
-    # save api key to secrets.json
-    os.system("touch secrets.json")
+    # save API key to secrets.json
+    if os.path.exists("secrets.json"):
+        with open("secrets.json", "r") as fp:
+            secrets = json.load(fp)
+    else:
+        secrets = {}
+    secrets["OPENAI_API_KEY"] = key_string
     with open("secrets.json", "w") as fp:
-        json_data = {"OPENAI_API_KEY": key_string}
-        json.dump(json_data, fp, sort_keys=True, indent=4)
+        json.dump(secrets, fp, sort_keys=True, indent=4)
 
 
 def openai_api_key_exists():
@@ -33,7 +36,7 @@ def check_base_reqs() -> bool:
     return_val = True
     msg = ""
     if shutil.which("uv") is None:
-        msg += "install uv tp continue: https://docs.astral.sh/uv/getting-started/installation/.\n"
+        msg += "install uv to continue: https://docs.astral.sh/uv/getting-started/installation/.\n"
         return_val = False
     if shutil.which("tmux") is None:
         msg += "tmux not found; install tmux to continue.\n"
@@ -47,45 +50,36 @@ def check_base_reqs() -> bool:
     return return_val
 
 
-def install_reqs():
-    """Install python requirements with uv."""
-    output = subprocess.run(["uv", "sync"], capture_output=True, text=True)
-    print(output.stdout)
-
-
 def main():
-    print("This script will guide you through installation.\n")
+    print("This script will guide you through installation...\n")
     if not check_base_reqs():
         return
 
     if not os.path.exists("secrets.json"):
         set_openai_key("")
     if not openai_api_key_exists():
-        resp = input("add openai_api_key to `secrets.json`:\n")
+        resp = input("add OPENAI_API_KEY to ./secrets.json:\n")
         if resp:
             set_openai_key(resp)
 
-    print("installing python environment...")
-    install_reqs()
-
     while True:
-        shell_file = input(
-            "Provide a shell file path to add the `chat` function (e.g. ~/.bashrc, ~/.zshrc), or return to skip:"
+        shell_path = input(
+            "Provide a shell file path to add the `chat` function (e.g. ~/.bashrc, ~/.zshrc), or return to skip:\n"
         )
-        shell_file = os.path.expanduser(shell_file)
-        if shell_file == "":
+        shell_path = os.path.expanduser(shell_path)
+        if shell_path == "":
             print("chat command not added.")
             break
-        elif os.path.exists(shell_file):
+        elif os.path.exists(shell_path):
             try:
-                with open(shell_file, "a") as shell_file:
-                    shell_file.write(bash_func_str)
-                    print(f"`chat` function added to {shell_file}.")
+                with open(shell_path, "a") as fp:
+                    fp.write(bash_func_str)
+                    print(f"`chat` function added to {shell_path}.")
                     break
             except Exception as e:
-                print(f"error writing to {shell_file}: {e}")
+                print(f"error writing to {shell_path}: {e}")
         else:
-            print("shell file not found; try again.")
+            print(f"shell file not found at {shell_path}.")
 
     print("installation complete.\n\nOpen a new shell session and type `chat`!")
 
